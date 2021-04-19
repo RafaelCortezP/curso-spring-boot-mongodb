b1b1604293122b53475b65b6f5b7d3b8220aa249
Rafael Cortez <rafaelcortezp@gmail.com>
2021-04-19 18:16:00 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/rafaelcortez/cursospringbootmongodb/CursospringbootmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.rafaelcortez.cursospringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringbootmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringbootmongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rafaelcortez/cursospringbootmongodb/CursospringbootmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelcortez.cursospringbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringbootmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
